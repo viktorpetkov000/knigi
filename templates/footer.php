d383c06a10586cfe935fba76a34b70c5f31656d8
viktorpetkov000 <viktor.petkov000@gmail.com>
2023-05-30 12:43:09 +0300

    Split files

---
+++ templates/footer.php
@@ -0,0 +1,18 @@
+<div class="footer">
+  <div class="footer-info">
+    <div class="footer-logo">
+      <img src="assets/logo2.png"/>
+    </div>
+    <div class="footer-info2">
+      <div class="footer-sitename">knigiknigi.knig</div>
+      <div class="footer-copyright">Copyright © 2021 Всички права запазени</div>
+    </div>
+    <div class="footer-slogan">Най-добрия слоган на света!</div>
+  </div>
+  <div class="footer-menu">
+    <a class="footer-menu-item" href="#">Контакт</a>
+    <a class="footer-menu-item" href="#">Услуги</a>
+    <a class="footer-menu-item" href="#">Лични данни</a>
+    <a class="footer-menu-item" href="#">Политика за използване на бисквитки</a>
+  </div>
+</div>
